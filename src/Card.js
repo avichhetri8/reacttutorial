434ee4647fc7f98af48b2bec7eda2a40873dadbc
Avi <avichhetri8@gmail.com>
2020-12-10 11:55:09 +0545

    children , props example

---
+++ src/Card.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import logo from './logo.svg';
+import './App.css';
+
+export const Card = (props) => {
+    return (
+        <div className="card">
+            {console.log("p",props)}
+            
+      <img  src={logo} alt="Avatar" style={{width:"100%" }} />
+      <div className="container">
+        <h4><b>{props.name}</b></h4> 
+        <p>{props.title}</p> 
+        <p>{props.children}</p>
+      </div>
+    </div>
+    )
+}
+
