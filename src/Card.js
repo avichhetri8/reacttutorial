33d6c08905791e4b81a0f81fa9bd63dc0ea1c9e2
Avi <avichhetri8@gmail.com>
2020-12-14 16:20:38 +0545

    card to class component

--- src/Card.js
+++ src/Card.js
@@ -2,22 +2,26 @@ import React from 'react'
 import logo from './logo.svg';
 import './App.css';
 
-export const Card = (props) => {
-    console.log("",props)
+export class Card extends React.Component{
+    constructor(){
+      super()
+    }
+    render(){
     return (
         <div className="card">
-            {console.log("p",props)}
+            {console.log("class",this.props)}
             
-      <img  src={props.avatar} alt="Avatar" style={{width:"100%" }} />
+      <img  src={this.props.avatar} alt="Avatar" style={{width:"100%" }} />
       <div className="container">
-        <h4><b>{props.name}</b></h4> 
-        <div><input type="text" onChange={props.onChange}/></div>
-        <p>{props.title}</p> 
-        <div>{props.children}</div>
+        <h4><b>{this.props.name}</b></h4> 
+        <div><input type="text" onChange={this.props.onChange}/></div>
+        <p>{this.props.title}</p> 
+        <div>{this.props.children}</div>
         
-        <div><button className="button-red button" onClick={props.onDelete}>Delete</button></div>
+        <div><button className="button-red button" onClick={this.props.onDelete}>Delete</button></div>
       </div>
     </div>
     )
+}
 }
 
