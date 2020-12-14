fafae209b8ba13b53a16aa1f3e3ba00659dfb6dd
Avi <avichhetri8@gmail.com>
2020-12-14 16:16:02 +0545

    app Class based

--- src/App.js
+++ src/App.js
@@ -1,76 +1,96 @@
+
+import React, {Component} from 'react'
 import {Card} from './Card.js'
-import {useState} from 'react'
 import logo from './logo.svg';
 
-function App() {
-  const [name, setName] = useState("Abishek")
-  const [showCard, setShowCard] = useState(true)
-  const [cardButton, setcardButton] = useState("Hide")
-  const [cards, setCards] = useState([{ 
-    id:1,
-    name:"Ram", 
-    title:"Engineer",
-    avatar : logo
-  },
-  {
-    id:2,
-    name:"Shayam" ,
-    title:"Computer Engineer",
-    avatar : logo
+class App extends Component {
+  constructor(props) {
+    console.log("dagatat")
+    super(props);
+    this.toggleShowCard = this.toggleShowCard.bind(this)
+    this.ChangeNameHandler = this.ChangeNameHandler.bind(this)
+    this.ChangeInputHandler = this.ChangeInputHandler.bind(this)
+    this.onChangeHanlder = this.onChangeHanlder.bind(this)
+    this.DeleteHandler = this.DeleteHandler.bind(this)
+   
+    this.state = {
+        cards:[
+        { 
+          id:1,
+          name:"Ram", 
+          title:"Engineer",
+          avatar:logo
+        },
+        {
+          id:2,
+          name:"Shayam" ,
+          title:"Computer Engineer",
+          avatar:logo
+        }
+      ],
+      showCard : true,
+      cardButton : 'Hide'
+      };
+     
+  }
+  // const [name, setName] = useState("Abishek")
+  // setShowCard = this.setuseState(true)
+  // const [cardButton, setcardButton] = useState("Hide")
+  // const [cards, setCards] = useState()
+  toggleShowCard = () => {
+    this.setState({showCard: !this.state.showCard})
+    this.state.showCard ? this.setState({cardButton : "Show"}) : this.setState({cardButton : "Hide"})
+ 
+  }
+
+        
+  
+  componentWillUnmount(){
+   
+  }
+  ChangeNameHandler = (name) =>{
+    this.setState({name: name})
 
   }
-])
-  const toggleShowCard = () =>{
-    showCard? setcardButton("Show") : setcardButton("Hide");
-    setShowCard(!showCard)
-    
-  }
-  const ChangeNameHandler = (name) =>{
-    setName(name)
-
-  }
-  const DeleteHandler = (cardIndex)=> {
-      const copy_card = [...cards]
+  DeleteHandler = (cardIndex)=> {
+      const copy_card = [...this.state.cards]
       console.log("c1",copy_card)
       copy_card.splice(cardIndex,1)
       console.log("c2",copy_card)
-      setCards(copy_card)
+      this.setState({cards:copy_card})
   }
 
-  const ChangeInputHandler = event => setName(event.target.value)
+  ChangeInputHandler = event =>  this.setState({name: event.target.value})
 
-  const onChangeHanlder= (event,key)=>{
+  onChangeHanlder= (event,key)=>{
     //to find the card
-    const cardIndex = cards.findIndex(card=>card.id == key)
+    const cardIndex = this.state.cards.findIndex(card=>card.id === key)
     console.log("key",key, cardIndex)
     //to copy
-    const copy_card = [...cards]
+    const copy_card = [...this.state.cards]
     //
     copy_card[cardIndex].name = event.target.value
-    setCards(copy_card)
+    this.setState({cards:copy_card})
   }
-  const cardMark = showCard &&  cards.map((card,index) => (<Card 
+
+  
+  render(){
+    const cardMark = this.state.showCard &&  this.state.cards.map((card,index) => (<Card 
       name={card.name} avatar={card.avatar} title={card.title}
       key={card.id}
-      onDelete={()=>{DeleteHandler(index)}}
-      onChange={(event)=>onChangeHanlder(event,card.id)}
+      onDelete={()=>{this.DeleteHandler(index)}}
+      onChange={(event)=>this.onChangeHanlder(event,card.id)}
             >
         hello
     </Card>
   ))
-  const buttonMark = (
-    <div>
-      <button className="button">Green</button>
-      <button className="button button2">Blue</button>
-    </div>
-  )
-
   return (
     <div >
-      <button className="button" onClick={toggleShowCard}>{cardButton}</button>
+      <button className="button" onClick={this.toggleShowCard.bind(this)}>{this.state.cardButton}</button>
            {cardMark}
     </div>
   );
+}
 }
 
 export default App;
